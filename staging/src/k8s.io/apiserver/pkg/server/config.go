16dc778430ef55d8f2c85a6cd8ff67898c01702f
Abu Kashem <akashem@redhat.com>
2021-08-31 18:11:15 -0400

    UPSTREAM: <carry>: change opt-in due to upstream revert
    openshift-rebase(v1.24):source=4954e48523c

--- staging/src/k8s.io/apiserver/pkg/server/config.go
+++ staging/src/k8s.io/apiserver/pkg/server/config.go
@@ -944,7 +944,7 @@ func DefaultBuildHandlerChain(apiHandler http.Handler, c *Config) http.Handler {
 	if c.CompressionDisabledFunc != nil {
 		handler = genericapifilters.WithCompressionDisabled(handler, c.CompressionDisabledFunc)
 	}
-	handler = genericfilters.WithOptInRetryAfter(handler, c.newNotReadyRetryAfterFunc())
+	handler = genericfilters.WithOptInRetryAfter(handler, c.newServerFullyInitializedFunc())
 	handler = genericfilters.WithHTTPLogging(handler, c.newIsTerminatingFunc())
 	if utilfeature.DefaultFeatureGate.Enabled(genericfeatures.APIServerTracing) {
 		handler = genericapifilters.WithTracing(handler, c.TracerProvider)
